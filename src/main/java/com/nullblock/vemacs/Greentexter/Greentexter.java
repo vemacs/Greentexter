c5a1a118591c19d5d81a03415c2a7713ab1ec86c
vemacs <gentoo@outlook.com>
2013-03-23 09:50:47 -0600

    Indentation

--- src/main/java/com/nullblock/vemacs/Greentexter/Greentexter.java
+++ src/main/java/com/nullblock/vemacs/Greentexter/Greentexter.java
@@ -3,13 +3,13 @@ package com.nullblock.vemacs.Greentexter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Greentexter extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        new GreentexterListener(this);
-    }
-    @Override
-    public void onDisable() {        
-        
-    }
+	@Override
+	public void onEnable() {
+		new GreentexterListener(this);
+	}
+	@Override
+	public void onDisable() {        
+
+	}
 
 }
--- src/main/java/com/nullblock/vemacs/Greentexter/GreentexterListener.java
+++ src/main/java/com/nullblock/vemacs/Greentexter/GreentexterListener.java
@@ -1,5 +1,5 @@
 package com.nullblock.vemacs.Greentexter;
- 
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -7,20 +7,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class GreentexterListener implements Listener {
- 
-    private Greentexter plugin;
- 
-    public GreentexterListener(Greentexter greentexter) {
-        plugin = greentexter;
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
-    }
- 
-        @EventHandler(priority = EventPriority.LOW)
-        public void onPlayerChat(AsyncPlayerChatEvent event) {
-        	String eventMessage = event.getMessage();
-        	if(eventMessage.startsWith(">")){
-            	event.setMessage(ChatColor.GREEN + eventMessage);
-        	}
-    	}
- }
- 
+
+	private Greentexter plugin;
+
+	public GreentexterListener(Greentexter greentexter) {
+		plugin = greentexter;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+	@EventHandler(priority = EventPriority.LOW)
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		String eventMessage = event.getMessage();
+		if(eventMessage.startsWith(">")){
+			event.setMessage(ChatColor.GREEN + eventMessage);
+		}
+	}
+}
